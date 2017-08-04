810975d23150658c946632c327c7e7d1e0cc302d
Collins Arku Yao <arkucollins@gmail.com>
2017-08-04 11:59:13 +0000

    Create PointerArray.c

---
+++ Pointers/PointerArray.c
@@ -0,0 +1,15 @@
+//Pointers and Single-dimensional Arrays
+
+#include<stdio.h>
+void main()
+{
+int ary[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+int i;
+for (i = 0; i < 10; i ++)
+{
+
+    printf("\n i = %d , ary[i] = %d , *(ary+i)= %d ", i, ary[i], *(ary + i));
+    printf("&ary[i] = %X , ary + i = %X", &ary[i], ary+i);
+
+}
+}
